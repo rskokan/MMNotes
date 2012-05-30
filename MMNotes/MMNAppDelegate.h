7b606dcb38edeea02b06cc06aab3f371c3b2a362
Radek Skokan <radek@ramac.local>
2012-05-30 12:00:20 +0200

    Initial Commit

---
+++ MMNotes/MMNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MMNAppDelegate.h
+//  MMNotes
+//
+//  Created by Radek Skokan on 5/30/12.
+//  Copyright (c) 2012 radek@skokan.name. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MMNotes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MMNotes
+//
+//  Created by Radek Skokan on 5/30/12.
+//  Copyright (c) 2012 radek@skokan.name. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MMNAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MMNAppDelegate class]));
+    }
+}
